eb2977b1591d45de91c8a663f7982fdd28f9d935
Petr Noga <n.skypo@gmail.com>
2017-05-08 13:47:30 +0200

    New stuff
    Some new stuff added, but must to be improve function and make css fro
    all about searching i dont know how to make search with normize css..
    Tonight will be long :{

--- public_html/js/weather.js
+++ public_html/js/weather.js
@@ -6,21 +6,56 @@
 var url = 'http://api.openweathermap.org/data/2.5/weather?q=Ostrava&units=metric&lang=cz&appid=abcb921ca7be6867b0b964ece67ac025';
 
 $(document).on("pagecreate", "#page1", function (event) {
+   
     getWeatherToday(url);
     searchName();
 });
+
+function Empty(){
+    $('.weather-place').empty();
+    $('.weather-temperature').empty();
+    $('.weather-min-temperature').empty();
+    $('.weather-max-temperature').empty();
+    $('.weather-description').empty();
+    $('.weather-humidity').empty();
+    $('.weather-wind-speed').empty();
+    $('.weather-sunrise').empty();
+    $('.weather-sunset').empty();  
+    
+}
+
 function getWeatherToday(url) {
     $.ajax({
         dataType: "json",
         url: url,
         success: function (data) {
-            $('#Actual').empty();
-            $('#Actual').append(data["name"] + " " + data["main"].temp + "°C <br>");
-            $('#Actual').append("Popis: "+data["weather"][0].description);
+            var index = localStorage.length;
+            
+            var SunriseSec = data["sys"].sunrise;
+            var SunriseDate = new Date(SunriseSec * 1000);
+            var SunriseTimestr = SunriseDate.toLocaleTimeString();
+            
+            var SunsetSec = data["sys"].sunset;
+            var SunsetDate = new Date(SunsetSec * 1000);
+            var SunsetTimestr = SunsetDate.toLocaleTimeString();
+            
+           // Empty();
+            $('.place').append(data["name"]);
+            $('.temperature').append(data["main"].temp);
+            $('.weather-max-temperature').append(data["main"].temp_max+"°C");
+            $('.desc').append(data["weather"][0].description);
+            $('.weather-humidity').append(data["main"].humidity+"%");
+            $('.weather-wind-speed').append(data["wind"].speed+" m/s");
+            $('.weather-sunrise').append(SunriseTimestr);
+            $('.weather-sunset').append(SunsetTimestr);     
 
-            localStorage.setItem("Current",data["name"]);
+            localStorage["Mesto"+index]="Město: "+data["name"] + ", Teplota " + data["main"].temp + "°C";
+                
+            index++;
         }
     });
+    var today= new Date();
+    $('.today').append(today.toDateString());
 }  
 function searchName() {
    var pom = "";
